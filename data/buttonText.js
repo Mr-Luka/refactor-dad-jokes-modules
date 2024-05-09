f5f403abf3c7913deead3e239565c4a1e5d62172
Luka <luka.luka.luka93@gmail.com>
2024-05-09 15:03:33 -0700

    made different .js files and put the code in each of them, now need to import them

---
+++ data/buttonText.js
@@ -0,0 +1,14 @@
+// Default export, only one
+
+const buttonText = [
+  'Ugh.',
+  '🤦🏻‍♂️',
+  'omg dad.',
+  'you are the worst',
+  'seriously',
+  'stop it.',
+  'please stop',
+  'that was the worst one',
+];
+
+export default buttonText;
